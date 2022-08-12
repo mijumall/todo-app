3cb297a92150693be96125379a45a33e1357e886
Ichigo Hoshimiya <ichigo@example.com>
2022-08-12 04:23:53 +0000

    [svelte] Set up svelte dev environment

---
+++ svelte/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  console.log("Hi")
+</script>
+
+<main>
+  Hi
+</main>
+
+<style lang="scss">
+  main {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    overflow: scroll;
+  }
+</style>
